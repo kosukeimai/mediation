2ecfda7249181cf030f81fceee40d6faed3e72a7
luke <luke@90758a3b-f49f-4eee-9887-1358e2851c59>
2009-02-13 14:23:06 +0000

    no message

--- R/Binary-Function-Testing.R
+++ R/Binary-Function-Testing.R
@@ -70,21 +70,8 @@ M[latentm <  1] <- 0
 M[latentm >= 1] <- 1
 Y <- alpha.3 + beta.3*T + gamma*M + beta.2*X.1 + beta.2*X.2 + rnorm(n)
 
-#Standard Method - With Covariates
 mmodel <- glm(M ~ T + X.1 + X.2, family=binomial(link=probit))
-alpha2.est<- mmodel$coef[1]
-beta2.est <- mmodel$coef[2]
 ymodel <- lm(Y ~ T + M + X.1 + X.2)
-gamma.est <- ymodel$coef[3]
-beta3.est <- ymodel$coef[2]
-delta.true <- gamma*(pnorm(alpha.2 + beta.2) - pnorm(alpha.2))
-tau.true <- beta.3 + gamma*(pnorm(alpha.2 + beta.2) - pnorm(alpha.2))
-pr.true <- delta.true/tau.true
-delta.t.est <- gamma.est*(pnorm(alpha2.est + beta2.est) - pnorm(alpha2.est))
-scalar_Li <- pnorm(mmodel$coef[1] + mmodel$coef[2])
-delta.est <- gamma.est*beta2.est*scalar_Li
-tau.est <- beta3.est + gamma.est*(pnorm(alpha2.est + beta2.est) - pnorm(alpha2.est))
-pr.est <-  delta.est/tau.est
 
 #New Methods
 time.start <- Sys.time()
@@ -95,11 +82,85 @@ time.start <- Sys.time()
 mod.2 <- mediate.binary(mmodel, ymodel, sims=1000)
 print (round((Sys.time()-time.start),1))
 
-cat("True Point Estimate: ", delta.true, "\n")
-cat("True Proportion Mediated: ", pr.true, "\n" )
-cat("Point Estimate Li Method: ", delta.est, "\n")
-cat("Proportion Est Li Method: ", pr.est, "\n")
 summary(mod.1)
 summary(mod.2)
 
+#############################################
+#             Binary Outcome                #
+#############################################
+rm(list=ls())
+set.seed(373456)
+source("binary.med.R")
+
+#Simulate X Vars
+n <-  1000
+T <- round(runif(n), 0)
+#Population Values
+alpha.2 <- .25
+alpha.3 <- .25
+beta.2 <- .25
+beta.3 <- .25
+gamma <- .25
+
+M <- alpha.2 + beta.2*T + rnorm(n)
+latenty <-  alpha.3 + beta.3*T + gamma*M + rnorm(n)
+Y <- latenty
+Y[latenty <  1] <- 0
+Y[latenty >= 1] <- 1
+
+#Standard Method - Without Covariates
+ymodel <- glm(Y ~ T + M, family=binomial(link=probit))
+mmodel <- lm(M ~ T)
+
+#New Methods
+time.start <- Sys.time()
+mod.1 <- mediate.binary(mmodel, ymodel, sims=1000, boot=TRUE)
+print (round((Sys.time()-time.start),1))
+
+time.start <- Sys.time()
+mod.2 <- mediate.binary(mmodel, ymodel, sims=1000)
+print (round((Sys.time()-time.start),1))
+
+summary(mod.1)
+summary(mod.2)
+
+#############################################################
+#                     With Covariates                       #
+#############################################################
+rm(list=ls())
+set.seed(3)
+source("binary.med.R")
+
+n <-  1000
+#Simulate X Vars
+T <- round(runif(n), 0)
+X.1 <- rnorm(n)
+X.2 <- rnorm(n)
+#Population Values
+alpha.2 <- .25
+alpha.3 <- .25
+beta.2 <- .25
+beta.3 <- .25
+gamma <- .25
+
+M <- alpha.2 + beta.2*T + beta.2*X.1 + beta.2*X.2 + rnorm(n)
+latenty <-  alpha.3 + beta.3*T + gamma*M + beta.2*X.1 + beta.2*X.2 + rnorm(n)
+Y <- latenty
+Y[latenty <  1] <- 0
+Y[latenty >= 1] <- 1
+
+ymodel <- glm(Y ~ T + M + X.1 + X.2, family=binomial(link=probit))
+mmodel <- lm(M ~ T + X.1 + X.2)
+
+#New Method
+time.start <- Sys.time()
+mod.1 <- mediate.binary(mmodel, ymodel, sims=1000, boot=TRUE)
+print (round((Sys.time()-time.start),1))
+
+time.start <- Sys.time()
+mod.2 <- mediate.binary(mmodel, ymodel, sims=1000)
+print (round((Sys.time()-time.start),1))
+
+summary(mod.1)
+summary(mod.2)
 
