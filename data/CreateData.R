b1bbb5c88e2f3e081e7ae7bd9694e4ddeb03008f
dustin <dustin@90758a3b-f49f-4eee-9887-1358e2851c59>
2009-05-24 23:13:45 +0000

    no message

--- data/CreateData.R
+++ data/CreateData.R
@@ -1,4 +1,5 @@
 library(foreign)
+library(MASS)
 
 rm(list=ls())
 setwd("H:/imai_methods/Mediation/CheckoutCVS/mediation/analysis/example/JOBS")
@@ -9,7 +10,7 @@ save(job,file="job.RData")
 
 
 
-#Generate simulation data
+#Generate simulation data for mediation function
 rm(list=ls())
 
 n<-5000
@@ -27,12 +28,12 @@ KAPPA<-.25
 ETA.3<-.25
 ETA.2<-.25
 SIGMA.3 <- 1
-SIGMA.SQ<-SIGMA.3^2
+SIGMA.3.SQ<-SIGMA.3^2
+SIGMA.2 <- 1
+SIGMA.2.SQ<-1
 
-sigma22 <- 1
-sigma33 <- 1
 cov <- 0
-Sigma <- matrix(c(sigma22^2,cov,cov,sigma33^2), 2,2)
+Sigma <- matrix(c(SIGMA.2^2,cov,cov,SIGMA.3^2), 2,2)
 e <- mvrnorm(n, rep(0,2), Sigma)
 
 
@@ -40,7 +41,7 @@ e <- mvrnorm(n, rep(0,2), Sigma)
 X.1 <- rnorm(n)
 T <- round(runif(n), 0)
 M.cont <- ALPHA.2 + BETA.2*T + ETA.2*X.1 + e[,1]
-latentm <-  ALPHA.3 + BETA.3*T + GAMMA*M.cont + ETA.3*X.1+ e[,2]
+latentm <-  ALPHA.2 + BETA.2*T + ETA.2*X.1 + e[,2]
 M.dich <- latentm
 M.dich[latentm <  0] <- 0
 M.dich[latentm >= 0] <- 1
@@ -65,7 +66,43 @@ Y.dich[latenty.dich <  0] <- 0
 Y.dich[latenty.dich >= 0] <- 1
 
 
+#Analytic answers
+#Dichotomous outcomes
+IKY_DELTA<-function(alpha.2,alpha.3,beta.2,beta.3,gamma, eta.2, eta.3, X, sigma.2.sq) {
+std.scalar<-sqrt(sigma.2.sq*gamma^2+1)
+t<-0
+temp1<-pnorm( (alpha.3 + beta.3*t + eta.3*X+ gamma*(alpha.2+beta.2+eta.2*X))/(std.scalar) )
+temp2<-pnorm( (alpha.3 + beta.3*t + eta.3*X+ gamma*(alpha.2+eta.2*X))/(std.scalar) )
+delta.0 <-mean(temp1-temp2)
+t<-1
+temp1<-pnorm( (alpha.3 + beta.3*t + eta.3*X+ gamma*(alpha.2+beta.2+eta.2*X))/(std.scalar) )
+temp2<-pnorm( (alpha.3 + beta.3*t + eta.3*X+ gamma*(alpha.2+eta.2*X))/(std.scalar) )
+delta.1 <-mean(temp1-temp2)
+DELTA<-(delta.0+delta.1)/2
+return(DELTA)
+}
 
-sim<-cbind(T,X.1,M.cont,M.dich,M.ord,Y.cont,Y.dich,Y.ord)
+#USES EQUATION 85 and 86 of IKT
+IKY_TAU<-function(alpha2,alpha3,beta2,beta3,gamma, eta2, eta3, X, sigma.2.sq) {
+std.scalar <- sqrt(gamma^2*sigma.2.sq + 1)
+alpha1<-(alpha3+gamma*alpha2)/std.scalar
+beta1<-(beta3+gamma*beta2)/std.scalar
+eta1<-(eta3+eta2*gamma)/(std.scalar)
+TAU<-mean(pnorm(alpha1+beta1+eta1*X)-pnorm(alpha1+eta1*X))
+return(TAU)
+}
+
+ACME<-IKY_DELTA(ALPHA.2,ALPHA.3,BETA.2,BETA.3,GAMMA, ETA.2, ETA.3, X.1, SIGMA.2.SQ)
+TAU<-IKY_TAU(ALPHA.2,ALPHA.3,BETA.2,BETA.3,GAMMA, ETA.2, ETA.3, X.1, SIGMA.2.SQ)
+PROPMED<-ACME/TAU
+DichOutcomesAnswers<-cbind(ACME,TAU,PROPMED)
+
+ACME<-GAMMA*BETA.2
+TAU<-BETA.3
+ContContAnswers<-cbind(ACME,TAU)
+
+sim<-as.data.frame(cbind(T,X.1,M.cont,M.dich,M.ord,Y.cont,Y.dich,Y.ord))
+sim.answers<-cbind(DichOutcomesAnswers,ContContAnswers)
 setwd("H:/imai_methods/Mediation/CheckoutCVS/mediation/mediation/data")
 save(sim,file="sim.RData")
+save(sim.answers,file="sim.answers.RData")
