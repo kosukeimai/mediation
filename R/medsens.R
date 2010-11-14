fa9bb18e4206da97d26a7140c78d19c7a5ca530e
jaquilyn <jaquilyn@90758a3b-f49f-4eee-9887-1358e2851c59>
2010-11-14 22:40:14 +0000

    up

--- R/medsens.R
+++ R/medsens.R
@@ -141,8 +141,7 @@ medsens <- function(x, rho.by=.1, sims=1000, eps=sqrt(.Machine$double.eps), type
             }
             if("direct" %in% type){ # direct effects with interaction
             	z0[i,] <- y.coefs[paste(T.cat),]
-            	z1[i,] <- y.coefs[paste(T.cat),] + (m.coefs[paste(T.cat),] * y.coefs[paste(int.lab),])
-            	}
+            	z1[i,] <- y.coefs[paste(T.cat),] + m.coefs[paste(int.lab),]*(m.coefs[paste("(Intercept)"), ] + m.coefs[paste(T.cat), ] + m.coefs[-(match(c("(Intercept)", paste(T.cat)), names(model.m$coefficients))),]*X.1bar[-(match(c("(Intercept)", paste(T.cat)), names(X.1bar)))])            	
             	} else {if("indirect" %in% type){
             			d0[i,] <- m.coefs[paste(T.cat),]*y.coefs[paste(mediator),]
                 		d1[i,] <- m.coefs[paste(T.cat),]*y.coefs[paste(mediator),]
