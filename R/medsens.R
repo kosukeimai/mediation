9cdf71c57bf5e843247a4531ed845b5ee28e9bd9
jaquilyn <jaquilyn@90758a3b-f49f-4eee-9887-1358e2851c59>
2011-03-18 21:28:58 +0000

    up

--- R/medsens.R
+++ R/medsens.R
@@ -187,7 +187,6 @@ print(head(data))
         
         #Save Variance Estimates
         if(INT){if("indirect" %in% effect.type){
-#        		if(!is.na(m.bar.covts))
             d0.var[i,] <- (y.coefs[paste(mediator),] + 0*y.coefs[paste(int.lab),])^2*v.m[T.cat,T.cat] + m.coefs[paste(T.cat),]^2*(v.y[mediator,mediator] + 0*v.y[int.lab, int.lab] + 0*2*v.y[mediator, int.lab])
             d1.var[i,] <- (y.coefs[paste(mediator),] + y.coefs[paste(int.lab),])^2*v.m[T.cat,T.cat] + m.coefs[paste(T.cat),]^2*(v.y[mediator,mediator] + v.y[int.lab, int.lab] + 2*v.y[mediator, int.lab])
             }
