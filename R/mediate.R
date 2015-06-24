11c11f3de61db1889c1861de7271038a90125277
hirose <hirose@90758a3b-f49f-4eee-9887-1358e2851c59>
2015-06-24 01:34:00 +0000

    lme4::sigma -> stats::sigma

--- R/mediate.R
+++ R/mediate.R
@@ -710,7 +710,8 @@ mediate <- function(model.m, model.y, sims = 1000,
           M.RANEF1 <- M.ranef*var1 + M.RANEF1   # sum of (random effects*corresponding covarites)  
           M.RANEF0 <- M.ranef*var0 + M.RANEF0
         }
-        sigma <- lme4::sigma(model.m)
+        sigma <- if(getRversion() >= "3.3.0") stats::sigma else lme4::sigma
+        sigma <- sigma(model.m)
         error <- rnorm(sims*n, mean=0, sd=sigma)
         muM1 <- tcrossprod(MModel.fixef.sim, mmat.t) + M.RANEF1
         muM0 <- tcrossprod(MModel.fixef.sim, mmat.c) + M.RANEF0
